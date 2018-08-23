2c25a954bc828694812706144b85ffd711235ebe
katebarz <k.barzakovska@gmail.com>
2018-08-23 13:43:38 +0200

    navbar + first page

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ let sections = $('section'),
     nav_height = nav.outerHeight();
 
 $(window).on('scroll', function() {
-    let cur_pos = $(this).scrollTop() + 100;
+    let cur_pos = $(this).scrollTop();
 
     sections.each(function() {
         let top = $(this).offset().top - nav_height,
-            bottom = top + $(this).outerHeight();
+            bottom = top + $(this).outerHeight()-50;
 
         if (cur_pos >= top && cur_pos <= bottom) {
             nav.find('a').removeClass('active');
-            sections.removeClass('active');
-
-            $(this).addClass('active');
             nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            if ($(this).attr('id') > 1) {
+                $('nav').addClass('scrolled-nav');
+            } else {
+                $('nav').removeClass('scrolled-nav');
+            }
         }
-
     });
 });
 ///////////////////HIGHLIGHT MENU ON CLICK//////////////////
 nav.find('a').on('click', function() {
-/*  stopImmediatePropagation();
-*/
     let $el = $(this),
         id = $el.attr('href');
     $('html, body').animate({
@@ -33,8 +32,9 @@ nav.find('a').on('click', function() {
     return false;
 });
 //////////////////MENU TOGGLE////////////////////
-$('#toggle-btn').on('click',function(){
-  $('nav ul').toggleClass('active');
+$('#toggle-btn').on('click', function() {
+    $('nav ul').toggleClass('active');
+    $('#toggle-btn').toggleClass('menu-toggle_active');
 })
 ///////////////////TYPE WRITER/////////////////////
 let i = 0;
@@ -42,16 +42,16 @@ let txt = 'Lorem ipsum dummy text blabla.';
 let speed = 100;
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
-///////////////////////////////////////
-/*$(window).scroll(function(){
-    scrollTop = $(window).scrollTop();    
-    if (scrollTop > 500) {
-      $('.nav').addClass('scrolled-nav');
-    } 
-  })*/
+///////////////////SLIDE BUTTON////////////////////
+$("#slide-btn").on('click',function() {
+    $('html,body').animate({
+        scrollTop: $("#2").offset().top-50},
+        'slow');
+});
+/////////////////////////////////////////////

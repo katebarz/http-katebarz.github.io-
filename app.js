bef0d919c47112d251ecf1e71c07444473d23fd6
katebarz <k.barzakovska@gmail.com>
2018-09-05 15:34:41 +0200

    skills section

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function resizeToMinimum(){
+  var minimum    = [640, 480];
+  var current    = [window.outerWidth, window.outerHeight];
+  console.log(current)
+  var restricted = [];
+  var i          = 2;
+
+  while(i-- > 0){
+    restricted[i] = minimum[i] > current[i] ? minimum[i] : current[i];
+  }
+
+  window.resizeTo(current[0], current[1]);
+}
+
+window.addEventListener('resize', resizeToMinimum, false)
 ///////////////////HIGHLIGHT MENU ON SCROLL//////////////////
 let sections = $('section'),
     nav = $('nav'),
@@ -13,22 +28,26 @@ $(window).on('scroll', function() {
         if (cur_pos >= top && cur_pos <= bottom) {
             nav.find('a').removeClass('active');
             nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
-            $(this).attr('id') > 1?$('nav').addClass('scrolled-nav'): $('nav').removeClass('scrolled-nav');
+            $(this).attr('id') > 1 ? $('nav').addClass('scrolled-nav') : $('nav').removeClass('scrolled-nav');
         }
     });
-
+//////////////HIDE ELEMENTS ON SMALL SCREEN//////////
     function showText(x) {
-        if (x.matches && pageYOffset > 700) { // If media query matches
+        if (x.matches && pageYOffset > 150) { // If media query matches
             $('#text').hide();
-             pageYOffset > 1350?$('#slide-btn').hide():false;
+            pageYOffset > 350 ? $('#slide-btn').hide() : false;
         } else {
             $('#text').show();
             $('#slide-btn').show();
         }
     }
-    var x = window.matchMedia("(max-width: 991px)")
+    var x = window.matchMedia("(max-width: 480px)")
     showText(x) // Call listener function at run time
     x.addListener(showText)
+    if ($('nav ul').hasClass('active')) {
+        $('#text').hide();
+        $('#slide-btn').hide();
+    }
 });
 
 ///////////////////HIGHLIGHT MENU ON CLICK//////////////////
@@ -44,9 +63,16 @@ nav.find('a').on('click', function() {
 //////////////////MENU TOGGLE////////////////////
 $('#toggle-btn').on('click', function() {
     $('nav ul').toggleClass('active');
+    if ($('nav ul').hasClass('active')) {
+        $('#text').hide();
+        $('#slide-btn').hide();
+    } else {
+        $('#text').show();
+        $('#slide-btn').show();
+    }
     $('#toggle-btn').toggleClass('menu-toggle_active');
-    $('#text').fadeToggle("1000");
-    $('#slide-btn').fadeToggle("1000");
+    //$('#text').fadeToggle("1000");
+    //$('#slide-btn').fadeToggle("1000");
     //$('#text').toggleClass('text-toogle');
 })
 ///////////////////TYPE WRITER/////////////////////
